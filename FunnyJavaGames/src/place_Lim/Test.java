39c0e1fe03695d3c849ac7f837ed3c31bce09757
brownjoon <brownlim.jh@gmail.com>
2017-05-16 17:36:19 +0900

    sss
    Sss

--- FunnyJavaGames/src/place_Lim/Test.java
+++ FunnyJavaGames/src/place_Lim/Test.java
@@ -1,5 +1,30 @@
 package place_Lim;
 
-public class Test {
+class Animal {
+	String naaa = "ssss";
+	String name;
+	String a = "동물 울음소리는";
+	void hear(){
+		System.out.println(a);
+	}
+}
 
+class Mouse extends Animal {
+	public Mouse(String name){
+		super.name = name;
+	}
+	void hear(){
+		System.out.println(name+"의 울음소리 입니다.");
+	}
+}
+
+class cow {
+}
+
+public class Test {
+	public static void main(String[] args) {
+		Animal ssss = new Mouse("호랑이");
+		ssss.hear();
+		System.out.println(ssss.naaa);
+	}
 }
