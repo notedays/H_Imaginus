bdcfd244d1c52edabb433b7e484f7b118fba363e
notedays <notedays@naver.com>
2017-05-17 16:48:30 +0900

    끝말잇기게임

---
+++ FunnyJavaGames/src/place_Share/LastWordGame.java
@@ -0,0 +1,22 @@
+package place_Share;
+
+
+/*	# 끝말 잇기 게임
+ *  - 친구들과 함께 할 수 있도록 게임 시작 시 플레이 인원을 입력받고 해당 인원 수 만큼 Player 클래스 생성하기
+ *  - Player 클래스에는 String name, int point 가 들어감.
+ *  - LastWordGame 클래스 내에는 String lastWord, int gameCount 가 들어감.
+ */
+
+public class LastWordGame {
+	String lastWord;
+	int gameCount;
+	
+	public static void main(String[] args) {
+
+	}
+}
+
+class Player{
+	String name;
+	int point;
+}
